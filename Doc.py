dd4f1dcf686a5d761e816c3f25725f55014140ea
reza tanaki zadeh <47104040+tedco-learning@users.noreply.github.com>
2020-09-10 10:11:21 +0430

    Update Doc.py

--- Doc.py
+++ Doc.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
+import tkinter as tk
+from tkinter import messagebox
+root = tk.Tk()
+root.withdraw()
+width_img = 500
+heigth_img = 300
 
-width_img = 640
-heigth_img = 480
 
-# cap = cv2.VideoCapture("http://192.168.1.71:4747/video")
 # --- get video stream from ip camera
 cap = cv2.VideoCapture("http://192.168.1.71:4747/video")
 # --- set width and height for video
-cap.set(3, width_img)  # ---- setting for height
-cap.set(4, heigth_img)  # ---- setting for width
-cap.set(10, 160)  # ---- setting for brightness
+cap.set(3, heigth_img)  # ---- setting for height
+cap.set(4, width_img)  # ---- setting for width
+cap.set(10, 150)  # ---- setting for brightness
 
 
 # --- some process on each frame of vedeo
@@ -18,7 +21,7 @@ def preProcessing(img):
     # --- make gray scale
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # --- insert a Gaussian blur for removing noise
-    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 0.5)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 0.5)
     # --- find edge of each things
     imgCanny = cv2.Canny(imgBlur, 200, 200)
     kernel = np.ones((5, 5))
@@ -67,23 +70,35 @@ def getWrap(img, biggest):
     if biggest != []:
         biggest = reorder(biggest)
         pts1 = np.float32(biggest)
+        # pts1 = np.reshape(pts1 - np.reshape(np.array([[5, 5], [5, 5], [5, 5], [5, 5]]), (4, 1, 2)), (4, 1, 2))
+        # print(pts1,np.shape(pts1))
         pts2 = np.float32([[0, 0], [width_img, 0], [0, heigth_img], [width_img, heigth_img]])
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
         imgOutput = cv2.warpPerspective(img, matrix, (width_img, heigth_img))
-        imgCropped = imgOutput[20:imgOutput.shape[0] - 20, 20:imgOutput.shape[1] - 20]
+        imgCropped = imgOutput[10:imgOutput.shape[0] - 10, 10:imgOutput.shape[1] - 10]
         imgCropped = cv2.resize(imgCropped, (width_img, heigth_img))
     return imgCropped
 
 
+i = 0
 while True:
     bool_result, img = cap.read()
     img = cv2.resize(img, (width_img, heigth_img))
     img_countour = img
     imgThres, h = preProcessing(img)
     biggest = getCountours(imgThres)
-    print(biggest)
+    # print(biggest)
     wrap = getWrap(img, biggest)
+    wrap = cv2.resize(wrap, (width_img, heigth_img))
+
     cv2.imshow("image", img_countour)
-    cv2.imshow("wrap", wrap)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("wrap5", wrap)
+    k = cv2.waitKey(1)
+    if k == 113:
         break
+    elif k == 115:
+        print("Ok")
+        cv2.imwrite("./saved_images/saved_" + str(i) + ".jpg", wrap)
+        messagebox.showinfo("saved !!", "Your picture has been saved .")
+        i = i + 1
+        pass
